b5f469f382ae0432901dab55056a030d46f76eac
vijaylpersad <vijaylpersad@utexas.edu>
2022-02-05 21:39:05 -0600

    project-one

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ console.log('hello world')
 const tiles = Array.from(document.querySelectorAll('.tile'))
 
 let board = ['','','','','','','','','','','','','','','','']
+let playerOneScore = 0
+let playerTwoScore = 0
+const playerOneScore_span = document.querySelector('.playerOneScore')
+const playerTwoScore_span = document.querySelector('.playerTwoScore')
+const tile = document.getElementsByClassName('tile')
+
 
 const winningConditions = [
     [0,1,2,3],
@@ -14,6 +20,27 @@ const winningConditions = [
     [2,6,10,14],
     [3,7,11,15]
 ];
+//Column 1 [0, 4, 8, 12]
+//Column 2 [1, 5, 9, 13]
+//Column 3 [2, 6, 10, 14]
+//Column 4 [3, 7, 11, 15]
+
+
+//function to occur on event of click on tile:
+
+function playerMove() {
+    // if(tile.classList !== 'playerOneChoice') {
+    //     tile.classList.add('playerOneChoice')
+    // }
+    console.log('clicked')
+}
+
+tile.addEventListener('click', playerMove()) 
+
+// when you click on each square, a function should check if the bottom square in that column isnt displaying none. if it isnt displaying nothing, check the square above it, if it isnt displaying nothing, check above, if it is displaying nothing, fill with playerTurnColorCLass, log value in playerColorClass array
+//and check for winner: does current array(s) match winning combos, if yes announce winner, if no playerSwitch(), repeat the game loop  
+
+
 
 
 //userAction
@@ -28,6 +55,6 @@ const winningConditions = [
 
 
 //for each area (tile or column TBD) listen for click, and perform userAction function--include
-tiles.forEach((tile, index) => {
-    tile.addEventListener('click', )
-})
+// tiles.forEach((tile, index) => {
+//     tile.addEventListener('click', )
+// })
