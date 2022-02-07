9ae10edd444cf6e2a6fee2b82a5020d87f3453ec
vijaylpersad <vijaylpersad@utexas.edu>
2022-02-06 22:20:40 -0600

    project-one

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ let playerOneScore = 0
 let playerTwoScore = 0
 const playerOneScore_span = document.querySelector('.playerOneScore')
 const playerTwoScore_span = document.querySelector('.playerTwoScore')
-const tile = document.getElementsByClassName('tile')
+const tile = document.querySelectorAll('.tile') //doesnt work if i do querySelectorAll -- event listener error
+const div = document.querySelectorAll('.container > div')
+
+let playerMove = div.forEach(div => {
+    div.addEventListener('click', event => {
+        console.log(event)
+
+        // if(div.classList !== 'playerTwoChoice')
+        div.classList.add('playerOneChoice')
+    
+    // individual column click conditional function 
+        //if 0 4 8 12 are clicked, if 12 is empty, add classList to 12. if 12classList =${playerOneChoice OR playerTwoChoice}, move on
+        //if 8 isnt filled add classlist, else move on, if 4 isnt filled add classlist, else add classlist to 0 
+    
+        
+    })
+})
+//https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
 
 
 const winningConditions = [
@@ -28,14 +45,14 @@ const winningConditions = [
 
 //function to occur on event of click on tile:
 
-function playerMove() {
-    // if(tile.classList !== 'playerOneChoice') {
-    //     tile.classList.add('playerOneChoice')
-    // }
-    console.log('clicked')
-}
+// function playerMove() {
+//     if(div.classList !== 'playerOneChoice') {
+//         div.classList.add('playerOneChoice')
+//     }
+//     console.log('clicked')
+// }
 
-tile.addEventListener('click', playerMove()) 
+// tile.addEventListener('click', playerMove) 
 
 // when you click on each square, a function should check if the bottom square in that column isnt displaying none. if it isnt displaying nothing, check the square above it, if it isnt displaying nothing, check above, if it is displaying nothing, fill with playerTurnColorCLass, log value in playerColorClass array
 //and check for winner: does current array(s) match winning combos, if yes announce winner, if no playerSwitch(), repeat the game loop  
